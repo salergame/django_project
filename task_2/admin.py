31f455b203a2a703aa2d00cab8c0dca064fbc181
Sanzhar <salergame07@gmail.com>
2024-05-13 22:14:01 +0500

    task_5 admin_panel

--- task_2/admin.py
+++ task_2/admin.py
@@ -3,7 +3,41 @@ from django.contrib import admin
 from task_2.models import Client, Order, Product
 
 # Register your models here.
-admin.site.register(Client)
-admin.site.register(Product)
-admin.site.register(Order)
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    fields = [
+        "name",
+        "email",
+        "phone_number",
+        "address",
+        "registration_date",
+    ]
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name","quantity","price"]
+    list_editable = ["price",'quantity']
+    search_fields = ["name","description"]
+    list_filter = ["quantity","price"]
+    fields = [
+        "name",
+        "description",
+        "price",
+        "quantity",
+        "added_date",
+    ]
+    
+
+
+@admin.register(Order)
+class PaymentAdmin(admin.ModelAdmin):
+    fields = [
+        "client",
+        "products",
+        "total_amount",
+        "order_date",
+    ]
+
 
